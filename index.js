e16b007c006f790176ed7eebadd11d725f357e5c
lilleman <lilleman@larvit.se>
2018-08-21 14:32:17 +0200

    Using new log method and new lint rules

--- index.js
+++ index.js
@@ -1,62 +1,69 @@
 'use strict';
 
-const	topLogPrefix	= 'check-object-key: index.js: ',
-	log	= require('winston');
+const topLogPrefix = 'check-object-key: index.js: ';
+const LUtils       = require('larvitutils');
 
 /**
  * Checking an object key
  *
- * @param options {obj}	- {
+ * @param {obj} options {
  *		'obj':	object	- Object to have its keys checked
  *		'objectKey':	string	- object key name
  *		'default':	any	- The default value if it does not exist
  *		'defaultLabel':	string	- What to print in the log as the default value (will default to "default" if it is a string)
  *		'retries': integer	- used internally. Set to 10+ to have the method immediately set the default value or fail if the key does not exist
  *	}
- * @param cb {func} cb(err, waring) err = critical, warning for example setting default value
+ * @param {func} cb cb(err, waring) err = critical, warning for example setting default value
+ * @returns {obj} self
  */
 function checkObjKey(options, cb) {
-	let	logPrefix	= topLogPrefix + 'checkObjKey() - ',
-		warning;
+	let logPrefix = topLogPrefix + 'checkObjKey() - ';
+	let warning;
+	let log;
 
 	if (typeof cb !== 'function') {
-		cb	= function () {};
+		cb = function () {};
 	}
+
+	options = options || {};
 
 	if (typeof options !== 'object') {
-		const	err	= new Error('options must be an object');
-		log.error(logPrefix + err.message);
+		const err = new Error('options must be an object');
+
 		return cb(err);
 	}
 
-	if (typeof options.obj !== 'object') {
-		const	err	= new Error('options.obj must be an object');
-		log.error(logPrefix + err.message);
-		return cb(err);
+	if (! options.log) {
+		const lUtils = new LUtils();
+
+		options.log = new lUtils.Log();
 	}
+	log = options.log;
 
 	if (typeof options.objectKey !== 'string' || options.objectKey === '') {
-		const	err	= new Error('options.objectKey must be a non-emtpy string');
+		const err = new Error('options.objectKey must be a non-emtpy string');
+
 		log.error(logPrefix + err.message);
+
 		return cb(err);
 	}
 
-	if ( ! options.orgStack) {
-		const	err	= new Error('Stack');
+	if (! options.orgStack) {
+		const err = new Error('Stack');
 
-		options.orgStack	= err.stack;
+		options.orgStack = err.stack;
 	}
 
 	logPrefix += 'objectKey: "' + options.objectKey + '" - ';
 
 	if (options.retries === undefined) {
-		options.retries	= 0;
+		options.retries = 0;
 	}
 
 	if ((typeof options.default) === 'string' && options.defaultLabel === undefined) {
-		options.defaultLabel	= '"' + options.default + '"';
-	} else if ( ! options.defaultLabel) {
-		options.defaultLabel	= String(options.default);
+		options.defaultLabel = '"' + options.default + '"';
+	} else if (! options.defaultLabel) {
+		options.defaultLabel = String(options.default);
 	}
 
 	log.silly(logPrefix + 'retry: ' + options.retries);
@@ -66,12 +73,11 @@ function checkObjKey(options, cb) {
 		log.debug(logPrefix + 'obj["' + options.objectKey + '"] is set to a valid option, no modification needed');
 
 	// If no validValues is set, but objectKey is, assume it is a correct value and stop checking
-	} else if ( ! Array.isArray(options.validValues) && options.obj[options.objectKey] !== undefined) {
+	} else if (! Array.isArray(options.validValues) && options.obj[options.objectKey] !== undefined) {
 		log.debug(logPrefix + 'obj["' + options.objectKey + '"] is set, no modification needed');
 
 	// Exhaused amount of retries, do something radical
 	} else if (options.retries > 10) {
-
 		// Fallback to the default
 		if (Object.keys(options).indexOf('default') !== - 1) {
 			warning	= 'obj["' + options.objectKey + '"] is not set, setting default: ' + options.defaultLabel;
@@ -81,10 +87,12 @@ function checkObjKey(options, cb) {
 
 		// No default exists, return with error!
 		} else {
-			const	err	= new Error('obj["' + options.objectKey + '"] is not set, can not start.');
-			warning	= err.message;
+			const err = new Error('obj["' + options.objectKey + '"] is not set, can not start.');
+
+			warning = err.message;
 			log.error(logPrefix + err.message);
 			log.verbose(logPrefix + options.orgStack);
+
 			return cb(err, warning);
 		}
 
@@ -94,6 +102,7 @@ function checkObjKey(options, cb) {
 			options.retries ++;
 			checkObjKey(options, cb);
 		}, options.retries * 10);
+
 		return;
 	}
 
--- test/00test.js
+++ test/00test.js
@@ -1,128 +1,121 @@
 'use strict';
 
-const	checkObjKey	= require(__dirname + '/../index.js'),
-	test	= require('tape'),
-	log	= require('winston');
-
-// Set up winston
-log.remove(log.transports.Console);
-/** /log.add(log.transports.Console, {
-	'level':	'info',
-	'colorize':	true,
-	'timestamp':	true,
-	'json':	false
-});/**/
+const checkObjKey = require(__dirname + '/../index.js');
+const LUtils      = require('larvitutils');
+const test        = require('tape');
+const log         = new (new LUtils()).Log('none');
 
 test('should check if a value exists', function (t) {
-	const	obj	= {'foo': 'bar'};
+	const obj = {'foo': 'bar'};
 
-	checkObjKey({'obj': obj, 'objectKey': 'foo'}, function (err) {
+	checkObjKey({'obj': obj, 'objectKey': 'foo', 'log': log}, function (err) {
 		if (err) throw err;
 		t.end();
 	});
 });
 
 test('should check if a value exists that does not', function (t) {
-	const	obj	= {'foo': 'bar'};
+	const obj = {'foo': 'bar'};
 
-	checkObjKey({'obj': obj, 'objectKey': 'beng'}, function (err) {
-		t.equal(err instanceof Error,	true);
+	checkObjKey({'obj': obj, 'objectKey': 'beng', 'log': log}, function (err) {
+		t.equal(err instanceof Error, true);
 		t.end();
 	});
 });
 
 test('should set a default value', function (t) {
-	const	obj	= {'foo': 'bar'};
+	const obj = {'foo': 'bar'};
 
-	checkObjKey({'obj': obj, 'objectKey': 'beng', 'default': 'funk'}, function (err, warning) {
+	checkObjKey({'obj': obj, 'objectKey': 'beng', 'default': 'funk', 'log': log}, function (err, warning) {
 		if (err) throw err;
-		t.equal(obj.beng,	'funk');
-		t.equal(warning,	'obj["beng"] is not set, setting default: "funk"');
+		t.equal(obj.beng, 'funk');
+		t.equal(warning, 'obj["beng"] is not set, setting default: "funk"');
 		t.end();
 	});
 });
 
 test('should set a null default value', function (t) {
-	const	obj	= {'foo': 'bar'};
+	const obj = {'foo': 'bar'};
 
-	checkObjKey({'obj': obj, 'objectKey': 'beng', 'default': null}, function (err, warning) {
+	checkObjKey({'obj': obj, 'objectKey': 'beng', 'default': null, 'log': log}, function (err, warning) {
 		if (err) throw err;
-		t.equal(obj.beng,	null);
-		t.equal(warning,	'obj["beng"] is not set, setting default: null');
+		t.equal(obj.beng, null);
+		t.equal(warning, 'obj["beng"] is not set, setting default: null');
 		t.end();
 	});
 });
 
 test('should set an explicitly undefined default value', function (t) {
-	const	obj	= {'foo': 'bar'};
+	const obj = {'foo': 'bar'};
 
-	checkObjKey({'obj': obj, 'objectKey': 'beng', 'default': undefined}, function (err, warning) {
+	checkObjKey({'obj': obj, 'objectKey': 'beng', 'default': undefined, 'log': log}, function (err, warning) {
 		if (err) throw err;
-		t.equal(obj.beng,	undefined);
-		t.equal(warning,	'obj["beng"] is not set, setting default: undefined');
+		t.equal(obj.beng, undefined);
+		t.equal(warning, 'obj["beng"] is not set, setting default: undefined');
 		t.end();
 	});
 });
 
 test('should set a default value and label', function (t) {
-	const	obj	= {'foo': 'bar'};
+	const obj = {'foo': 'bar'};
 
 	checkObjKey({
-		'obj':	obj,
-		'objectKey':	'beng',
-		'default':	'funk',
-		'defaultLabel':	'booyah'
+		'obj':          obj,
+		'objectKey':    'beng',
+		'default':      'funk',
+		'defaultLabel': 'booyah',
+		'log':          log
 	}, function (err, warning) {
 		if (err) throw err;
-		t.equal(obj.beng,	'funk');
-		t.equal(warning,	'obj["beng"] is not set, setting default: booyah');
+		t.equal(obj.beng, 'funk');
+		t.equal(warning, 'obj["beng"] is not set, setting default: booyah');
 		t.end();
 	});
 });
 
 test('should fail if the given value is not in the validValues array', function (t) {
-	const	obj	= {'foo': 'bar', 'beng': 'torsk'};
+	const obj = {'foo': 'bar', 'beng': 'torsk'};
 
-	checkObjKey({'obj': obj, 'objectKey': 'beng', 'validValues': ['bisse', 'bosse']}, function (err) {
-		t.equal(err instanceof Error,	true);
+	checkObjKey({'obj': obj, 'objectKey': 'beng', 'validValues': ['bisse', 'bosse'], 'log': log}, function (err) {
+		t.equal(err instanceof Error, true);
 		t.end();
 	});
 });
 
 test('should validate a value in the validValues array', function (t) {
-	const	obj	= {'foo': 'bar', 'beng': 'torsk'};
+	const obj = {'foo': 'bar', 'beng': 'torsk'};
 
-	checkObjKey({'obj': obj, 'objectKey': 'beng', 'validValues': ['bisse', 'torsk']}, function (err) {
+	checkObjKey({'obj': obj, 'objectKey': 'beng', 'validValues': ['bisse', 'torsk'], 'log': log}, function (err) {
 		if (err) throw err;
-		t.equal(obj.beng,	'torsk');
+		t.equal(obj.beng, 'torsk');
 		t.end();
 	});
 });
 
 test('should return error if options is not an object', function (t) {
 	checkObjKey('foo', function (err) {
-		t.equal(err instanceof Error,	true);
+		t.equal(err instanceof Error, true);
 		t.end();
 	});
 });
 
 test('should return error if options.obj is not an object', function (t) {
-	checkObjKey({}, function (err) {
-		t.equal(err instanceof Error,	true);
+	checkObjKey({'log': log}, function (err) {
+		t.equal(err instanceof Error, true);
 		t.end();
 	});
 });
 
-test('should work even if cb is not given', function (t) {
+test('should not crash even if cb is not given', function (t) {
 	checkObjKey();
 	t.end();
 });
 
 test('should return error if objectKey is not valid', function (t) {
-	const	obj	= {'foo': 'bar'};
+	const obj = {'foo': 'bar'};
 
-	checkObjKey({'obj': obj, 'objectKey': {}}, function (err) {
-		t.equal(err instanceof Error,	true);
+	checkObjKey({'obj': obj, 'objectKey': {}, 'log': log}, function (err) {
+		t.equal(err instanceof Error, true);
 		t.end();
 	});
 });
